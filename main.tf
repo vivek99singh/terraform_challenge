97c68206adfd47e0bb9930e50eb4906fb2b0e14c
Vivek Singh <vivek.99.singh@gmail.com>
2020-06-23 21:04:00 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -57,13 +57,56 @@ resource "azurerm_app_service" "webapp1" {
 
   app_settings = {
     WEBSITE_NODE_DEFAULT_VERSION = "10.15.2"
-    ApiUrl = ""
-    ApiUrlShoppingCart = ""
-    MongoConnectionString = ""
-    SqlConnectionString = ""
+    ApiUrl = "/api/v1"
+    ApiUrlShoppingCart = "/api/v1"
+    MongoConnectionString = "mongodb://40.127.225.246:27020"
+    SqlConnectionString = "Server=tcp:productdbsql.database.windows.net,1433;Initial Catalog=productdb;Persist Security Info=False;User ID=adminsql;Password=Admin5ql@123456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     productImagesUrl = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
     Personalizer__ApiKey = ""
     Personalizer__Endpoint = ""
   }
 
+}
+
+resource "azurerm_sql_server" "sql_server" {
+  name                         = "productdbsql"
+  resource_group_name          = azurerm_resource_group.dev.name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "adminsql"
+  administrator_login_password = "Admin5ql@123456"
+}
+resource "azurerm_sql_firewall_rule" "sql_firewall" {
+  name                = "AllowAccessToAzure"
+  resource_group_name = azurerm_resource_group.dev.name
+  server_name         = azurerm_sql_server.sql_server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+resource "azurerm_sql_database" "sql_database" {
+  name                = "productdb"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = var.location
+  server_name         = azurerm_sql_server.sql_server.name
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "shoppingcartdb"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
+  ip_address_type     = "public"
+  dns_name_label      = "shoppingcartdb"
+  os_type             = "Linux"
+
+  container {
+    name   = "shoppingcartdb"
+    image  = "mongo:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 27017
+      protocol = "TCP"
+    }
+  }
 }
